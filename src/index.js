5af83fbc59fb937eaa46f31bb77d973cd7cb3fd2
ElenaStepanova <Elena_stepanova25@mail.ru>
2025-01-18 17:42:56 +0700

    Change init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import './GameSavingLoader';
-import './GameSaving';
-import './add';
-import './parser';
-import './reader';
+import Team from './Team';
+
+const team = new Team();
+
+const char1 = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10,
+};
+
+const char2 = {
+  name: 'Мечник',
+  type: 'Swordsman',
+  health: 60,
+  level: 1,
+  attack: 50,
+  defence: 15,
+};
+
+team.addCharacter(char1);
+team.addCharacter(char2);
+
+for (const character of team) {
+  console.log(character);
+}
---
+++ src/team.js
@@ -0,0 +1,17 @@
+class Team {
+  constructor() {
+    this.characters = [];
+  }
+
+  addCharacter(character) {
+    this.characters.push(character);
+  }
+
+  *[Symbol.iterator]() {
+    for (const character of this.characters) {
+      yield character;
+    }
+  }
+}
+
+export default Team;
